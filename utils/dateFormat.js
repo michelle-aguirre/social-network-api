6c31cde636bba0647e309f43237f52a9ac2ea8ae
michelle-aguirre <94195510+michelle-aguirre@users.noreply.github.com>
2022-05-25 17:46:09 -0400

    formatted date format

---
+++ utils/dateFormat.js
@@ -0,0 +1,36 @@
+const addDateSuffix = date => {
+    let dateStr = date.toString();
+
+    // get last char of date string
+    const lastChar = dateStr.charAt(dateStr.length - 1);
+
+    if (lastChar === '1' && dateStr !== '11') {
+        dateStr = `${dateStr}st`;
+    } else if (lastChar === '2' && dateStr !== '12') {
+        dateStr = `${dateStr}nd`;
+    } else if (lastChar === '3' && dateStr !== '13') {
+        dateStr = `${dateStr}rd`;
+    } else {
+        dateStr = `${dateStr}th`;
+    }
+
+    return dateStr;
+};
+
+// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+module.exports = (
+    timestamp,
+    { monthLength = 'short', dateSuffix = true } = {}
+) => {
+    let months;
+
+    if (monthLength === 'short') {
+        months = {
+            0: 'Jan',
+            1: 'Feb',
+            2: 'Mar',
+            3: 'Apr',
+            4: 'May',
+        }
+    }
+}
